7454284b482f8d6308d83802bfd914893789b268
John Steen <john@johnsteen.co>
2020-03-22 08:22:03 -0500

    Remove task list container

--- frontend/components/task_lists/task_list.jsx
+++ frontend/components/task_lists/task_list.jsx
@@ -1,13 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-const mapStateToProps = ({entities: { taskLists }}, ownProps) => {
-    return {}
-}
-
-const mapDispatchToProps = dispatch => {
-    return {}
-}
 
 class TaskList extends React.Component {
 
@@ -16,4 +8,4 @@ class TaskList extends React.Component {
     }
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(TaskList);
+export default TaskList;
